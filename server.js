ce72619ab373eade98222ddfacd1b4b178b7d328
Ihsan Husaeri <ihusaeri@gmail.com>
2019-09-30 13:34:23 +0700

    add node-cron module

--- server.js
+++ server.js
@@ -18,6 +18,7 @@
 	const Express = require( 'express' );
 	const Mongoose = require( 'mongoose' );
 	const timeout = require( 'connect-timeout' );
+	const NodeCron = require( 'node-cron' );
 
 	// Primary Variable
 	const App = Express();
@@ -56,16 +57,26 @@
 		useNewUrlParser: true,
 		ssl: config.database.ssl
 	} ).then( () => {
-		console.log( "Database connected (" + config.app.env + ")" );
+		console.log( "Database :" );
+		console.log( "\tStatus \t\t: Connected" );
+		console.log( "\tMongoDB URL \t: " + config.database.url + " (" + config.app.env + ")" );
 	} ).catch( err => {
-		console.log( "Database not connected (" + config.app.env + ")" );
+		console.log( "Database :" );
+		console.log( "\tDatabase Status : Not Connected" );
+		console.log( "\tMongoDB URL \t: " + config.database.url + " (" + config.app.env + ")" );
 	} );
 
 	// Server Running Message
 	var Server = App.listen( parseInt( config.app.port[config.app.env] ), () => {
 		Server.timeout = 120 * 60 * 1000;
-		console.log( "Server connected (" + config.app.env + ")" );
+		console.log( "Server :" );
+		console.log( "\tStatus \t\t: OK" );
+		console.log( "\tService \t: " + config.app.name + " (" + config.app.env + ")" );
+		console.log( "\tPort \t\t: " + config.app.port[config.app.env] );
 	} );
+
+	//scheduling job_update_transaksi_complete() with cron every monday
+	NodeCron.schedule('* * * * *')
 
 	// Routing
 	require( './routes/api.js' )( App );
